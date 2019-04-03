6567145ac5dfe53ef201a0f939ba7d339facd2e5
allanrodrigol <allanrodrigol@gmail.com>
2019-04-03 09:46:46 -0300

    Add files via upload

---
+++ fontes/servo-motors/servo-motors.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+ 
+#define SERVO_MOTOR 6
+ 
+Servo s;
+int p;
+ 
+void setup () {
+  Serial.begin(9600);
+  
+  s.attach(SERVO);
+  s.write(0);
+}
+ 
+void loop() {
+  //Gira servo motor para direita
+  for(p = 0; p < 90; p++) {
+    s.write(p);
+    delay(50);
+  }
+  
+  delay(1000);
+  
+  //Gira servo motor para esquerda
+  for(p = 90; p >= 0; p--) {
+    s.write(p);
+    delay(50);
+  }
+  
+  delay(1000);
+}
